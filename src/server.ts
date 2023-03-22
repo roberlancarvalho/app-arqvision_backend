f17bd699539160723cc3e5cf89986911cd1d07fe
roberlancarvalho <roberlan.carvalho@gmail.com>
2023-03-22 14:21:27 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+import { Router, Request, Response } from "express";
+
+const app = express();
+
+const route = Router();
+
+app.use(express.json());
+
+route.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello World with Typescript" });
+  console.log("Rodando na porta 3333")
+});
+
+app.use(route);
+
+app.listen(3333, () => "server running on port 3333");
